76bb261e91c42f7206bcdb2cf0223330803096ce
WILMAR-1 <wilmargc0@gmail.com>
2025-12-09 16:04:06 -0400

    Update display message with name and ID

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from GitHub Actions!');
+  res.send('Hello World from GitHub Actions! Wilmar Gómez de la Cruz 2024-0103');
 });
 
 app.listen(port, () => {
